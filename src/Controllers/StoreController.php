7eb78af3322741c6ce52e7685e8542c7f0f7bf76
谢日周 <932188443@qq.com>
2021-05-28 10:03:19 +0800

    'a'

--- src/Controllers/StoreController.php
+++ src/Controllers/StoreController.php
@@ -5,6 +5,7 @@ namespace Rizhou\Control\Controllers;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Rizhou\Control\Supply\StoreSynchronizing;
 
 class StoreController extends Controller
@@ -12,4 +13,25 @@ class StoreController extends Controller
     public function synchronizing(){
         StoreSynchronizing::make()->synchro();
     }
+
+    public function getCity(){
+        $city = StoreSynchronizing::make()->getCity();
+        return response()->json(json_encode($city,JSON_UNESCAPED_UNICODE));
+    }
+
+    public function getCounty($city_name){
+        $data = StoreSynchronizing::make()->getCounty($city_name);
+        return response()->json(json_encode($data,JSON_UNESCAPED_UNICODE));
+    }
+
+    public function getRoad($city_name,$county_name){
+        $data = StoreSynchronizing::make()->getRoad($city_name,$county_name);
+        return response()->json(json_encode($data,JSON_UNESCAPED_UNICODE));
+    }
+
+    public function getShop($city_name,$county_name,$road_name){
+        $data = StoreSynchronizing::make()->getShop($city_name,$county_name,$road_name);
+        return response()->json(json_encode($data,JSON_UNESCAPED_UNICODE));
+    }
+
 }
--- src/Routes/StoreNoticeRoute.php
+++ src/Routes/StoreNoticeRoute.php
@@ -8,5 +8,10 @@ class StoreNoticeRoute extends RouteAbstract
 {
     public function register(){
         $this->router->get('store-synchronizing','StoreController@synchronizing');
+
+        $this->router->get('get-store-city','StoreController@getCity');
+        $this->router->get('get-store-county','StoreController@getCounty');
+        $this->router->get('get-store-road','StoreController@getRoad');
+        $this->router->get('get-store-shop','StoreController@getShop');
     }
 }
